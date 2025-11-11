00c4431615789be7db9a55a7ce2306785abdf649
GeeGum065 <160366476+GeeGum065@users.noreply.github.com>
2025-11-11 11:25:13 -0300

    Update labirinto.c

--- labirinto.c
+++ labirinto.c
@@ -1,79 +1,102 @@
-#include <stdio.h>
+//Bibliotecas
 #include <stdlib.h>
-#include<unistd.h>
-#define tam 5
+#include <stdio.h>
+#include <stdbool.h>
+#include <time.h>
+#include <math.h>
 
-int dist(int x,int y){
-    int distancia = abs(x-3)+abs(y-4);
-    return distancia;
+//Parâmetros
+
+#define N 3
+
+//Números Aleatórios
+static inline int rand_int(int a, int b) {
+    /* inteiro uniforme em [a,b] (assume a <= b) */
+    return a + rand() % (b - a + 1);
 }
 
-int main()
+static inline double rand_unit() {
+    /* real uniforme em [0,1) */
+    return (double)rand() / (double)(RAND_MAX);
+}
 
+//Arrumar essa função, usar vetor[8/N*N-1] = {1,2,3,4,5,6,7,8}; para randomizar posições e povoar e a matriz aleatoriamente.
+void shuffle(int array[])
 {
-    
-    int v[2]={0,0},v_ant[2]={0,0};
-    char movimento;
-    char linha1[tam][tam]={{'.','.','.','#','.'},
-                        {'.','#','.','#','.'},
-                        {'.','#','.','.','.'},
-                        {'.','.','.','#','G'},
-                        {'.','#','.','.','.'}
-        
-    };
-    
-    
-    while (1){
-        
-        for(int i = 0; i<tam; i++){
-            for(int j=0;j<tam;j++){
-            if(i==v[0]&&j==v[1])
-                printf("S ");
-                
-            else
-                printf("%c ", linha1[i][j]);
-            }
-            printf("\n");
-        }
-        if(linha1[v[0]][v[1]]=='G')
+    int array[9] = {0,1,2,3,4,5,6,7,8};
+    srand(time(NULL));
+    for(int i = 0; i < N - 1; i++)
+    {
+        size_t j = rand() / (RAND_MAX / (N - i) + 1);
+        int t = array[j];
+        array[j] = array[i];
+        array[i] = t;
+    }
+}
+
+void quebraArray(int array[9], int mp[N][N])
+{
+    int cont = 0;
+    for(int i = 0; i < N; i++)
+    {
+        for(int j = 0; j < N; j++)
         {
-            printf("\nchegou");
-            break;
+            m[i][j] = array[cont];
+            cont++;
         }
+    }
+}
 
-    scanf("\n %c", &movimento);
-    printf("===============================\n");
-    v_ant[0]=v[0];
-    v_ant[1]=v[1];
-
-        switch(movimento){
-            case 'w':
-                v[0]--;
-                break;
-            case 's':
-                v[0]++;
-                break;
-            case 'a':
-                v[1]--;
-                break;
-            case 'd':
-                v[1]++;
-                break;
-            case 'q':
-                printf("programa terminado");
-                exit(1);
+//Protótipo/Esqueleto do 8 - Puzzle manual
+void povoarM(char m[N][N])
+{
+    int aux[9] = {0,1,2,3,4,5,6,7,8};
+    int mp[N][N];
+    shuffle(aux);
+    quebraArray(aux,m);
+    for(int i = 0; i < N; i++)
+    {
+        for(int j = 0; j < N; i++)
+        {
             
         }
-            if(linha1[v[0]][v[1]]=='#'||v[0]<0||v[1]<0||v[0]>=5||v[1]>=5){
-            v[0]=v_ant[0];
-            v[1]=v_ant[1];
-            printf("movimento invalido\n");
-            }
-    printf("a distancia eh: %d\n",dist(v[0],v[1]));
+    }
+}
 
 
+void imprimirM(char m[N][N])
+{
+    printf("-------------\n");
+    for(int i = 0; i < N; i++)
+    {
+        for(int j = 0; j < N; j++)
+        {
+            printf("| %c ",m[i][j]);
+            
+        }
+        printf("|\n-------------\n");
+    }
+}
+
+void andarJ(int cord[2]);
+
+
+// Protótipo/Esqueleto do 8 - Puzzle 
+int main()
+{
+    //char tabu[N][N]; int v[9];
+    int v[9] = {-2,-2,-2,-2,-2,-2,-2,-2,-2};
+
+    
+    //{{'1','2','3'},{'4','5','6'},{'7','8','.'}};
+    //povoarM(tabu);
+    //imprimirM(tabu);
+    
+    shuffle(v);     
+    for(int i = 0; i < 9; i++)
+    {
+        printf("%d",v[i]);
     }
 
     return 0;
-
 }
